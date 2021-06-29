76fc25e0012c9d3e4fe26acd5ae635f7ccacdd0e
Lucian Louw <lucian_louw@hotmail.com>
2021-06-29 15:25:45 +0200

    Updated

--- vermillion/settings/production.py
+++ vermillion/settings/production.py
@@ -29,12 +29,9 @@ from sentry_sdk.integrations.django import DjangoIntegration
 sentry_sdk.init(
     dsn="https://326c36f68f9d4cec9ccc270df613268c@o886548.ingest.sentry.io/5838012",
     integrations=[DjangoIntegration()],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
+    
     traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
+    
     send_default_pii=True 
 )
 
